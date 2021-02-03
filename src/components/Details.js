8cca1a87c7eb73a41ea63850068b0e9bafd9d54e
meherafroz <meherafroz6331@gmail.com>
2021-02-03 22:57:16 +0600

    details

--- src/components/Details.js
+++ src/components/Details.js
@@ -10,7 +10,7 @@ export default class Details extends Component {
         return (
             <div className="container py-5">
                 <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
-                    <h1>{product.title}</h1>
+                    <h1 className="text-title text-uppercase text-muted mt-3 mb-2">{product.title}</h1>
                 </div>
                 <div className="row">
                     <div className="col-10 mx-auto col-md-6 my-5">
@@ -19,11 +19,11 @@ export default class Details extends Component {
                         </div>
                     </div>
                     <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                        <h1 >Model : {product.title}</h1>  
-                        <h4 className="text-title text-uppercase text-muted mt-3 mb-2">Made by : {product.company}</h4>
+                        <h3 ><strong className="text-blue">Model :</strong> {product.title}</h3>  
+                        <h4 ><strong className="text-blue">Made by :</strong> {product.company}</h4>
                         <h4 ><strong className="text-blue">Price :</strong> {product.price}</h4>
                         <h4 ><strong className="text-blue">Info :</strong> {product.info}</h4>
-                        <h4><strong className="text-blue">InCart :</strong> {product.inCart}</h4>
+                        <h4><strong className="text-blue">InCart :</strong>{product.inCart?<ls> Added to cart</ls>:<l> Not in cart </l>}</h4>
                         <h4><strong className="text-blue">Count :</strong> {product.count}</h4>
                         <h4><strong className="text-blue">Total :</strong> {product.total}</h4>
                     </div>
